5a11802b5d500bc1b044be1f36b3ae9cc29f4f44
ivywu100 <wu.ivy100@gmail.com>
2024-08-09 23:54:02 -0700

    added custom commands

---
+++ uiTests/cypress/e2e/plantSeeds.cy.js
@@ -0,0 +1,13 @@
+describe("should plant seed in garden", () => {
+  before(() => {
+    cy.visitGarden();
+  });
+
+  it("should plant seed in each plot", () => {});
+
+  it("should harvest plants", () => {});
+
+  it("should plant seed with Plant All", () => {});
+
+  it("should harvest all plants with Harvest All", () => {});
+});
