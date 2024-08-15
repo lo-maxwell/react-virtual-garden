324ab71df0a1f118d7eae6f765296eac0428ccc4
ivywu100 <wu.ivy100@gmail.com>
2024-08-14 23:34:05 -0700

    removed UI Test Folder

--- uiTests/cypress/support/commands.js
+++
@@ -1,8 +0,0 @@
-Cypress.Commands.add("visitGarden", () => {
-  cy.visit("http://localhost:3000");
-  cy.wait(2000);
-
-  //wait for garden page to load
-  // cy.intercept("POST", "/_vercel/insights/view").as("pageLoaded");
-  // cy.wait("@pageLoaded");
-});
