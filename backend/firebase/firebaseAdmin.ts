c0ab87768894c5b8edd210db021abb7278d5cf07
Maxwell Lo <lo.maxwell8240@gmail.com>
2024-11-26 23:13:03 -0800

    add base64 encoding to firebase admin credentials to prevent json errors

--- backend/firebase/firebaseAdmin.ts
+++ backend/firebase/firebaseAdmin.ts
@@ -1,12 +1,16 @@
 import admin from 'firebase-admin';
 import * as path from 'path';
-
+const should_load_firebase_admin = process.env.ENABLE_FIREBASE;
+if (!should_load_firebase_admin || should_load_firebase_admin !== 'true') {
+  console.warn('Firebase is disabled');
+}
 // Initialize the Firebase Admin SDK (only once)
-if (typeof window === 'undefined') {
+else if (typeof window === 'undefined') {
   // const projectRoot = process.cwd();
   // const serviceAccountPath = path.resolve(projectRoot, 'secrets/firebase-service-account-file.json');
   if (!admin.apps.length) {
     console.log('Firebase Admin initialized');
+    
     const base64EncodedJson = process.env.FIREBASE_SERVICE_ACCOUNT;
     // Check if the environment variable is undefined or empty
     if (!base64EncodedJson) {
@@ -20,13 +24,6 @@ if (typeof window === 'undefined') {
     });
   } else {
     console.log('Firebase Admin already initialized');
-    // admin.apps.forEach((app) => {
-    //   if (app) app.delete();
-    //   console.log('Deleted existing Firebase Admin app');
-    // });
-    // admin.initializeApp({
-    //   credential: admin.credential.cert(serviceAccountPath),
-    // });
   }
 }
 // Rename the default export to 'firebaseAdmin'
