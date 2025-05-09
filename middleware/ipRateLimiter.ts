72fa0fb83f136a5105ddb4ec603605ec19638c41
Maxwell Lo <lo.maxwell8240@gmail.com>
2025-05-08 22:00:57 -0700

    remove account level rate limit since it broke some links

--- middleware/ipRateLimiter.ts
+++ middleware/ipRateLimiter.ts
@@ -1,12 +1,12 @@
-// middleware/rateLimit.ts
-const rateLimit = new Map<string, number[]>(); // Store timestamps of requests
 
-const RATE_LIMIT = 600; // max requests
+const ipRateLimit = new Map<string, number[]>(); // Store timestamps of requests
+
+const IP_RATE_LIMIT = 600; // max requests
 const TIME_WINDOW = 60 * 1000; // 1 minute in milliseconds
 
-export async function checkRateLimit(ip: string, buffer: number) {
+export async function checkIPRateLimit(ip: string, buffer: number) {
 	const currentTime = Date.now();
-	const timestamps = rateLimit.get(ip) || [];
+	const timestamps = ipRateLimit.get(ip) || [];
 
 	// Remove timestamps older than 60 seconds
 	const updatedTimestamps = timestamps.filter(timestamp => currentTime - timestamp < TIME_WINDOW);
@@ -15,11 +15,11 @@ export async function checkRateLimit(ip: string, buffer: number) {
 	updatedTimestamps.push(currentTime);
 
 	// Check if the number of requests in the last 60 seconds exceeds the limit
-	if (updatedTimestamps.length > RATE_LIMIT + buffer) {
+	if (updatedTimestamps.length > IP_RATE_LIMIT + buffer) {
 		return false; // Rate limit exceeded
 	}
 
 	// Update the stored timestamps
-	rateLimit.set(ip, updatedTimestamps);
+	ipRateLimit.set(ip, updatedTimestamps);
 	return true; // Within limit
 }
